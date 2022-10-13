19ceefac7d03dddd8fa3295ae27108da042f712a
bchaindeveloper <bcdev3@protonmail.com>
2022-10-13 18:59:49 -0400

    Box.sol

--- contracts/contract.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract valueSpot {
-    uint256 public value;
-    function setValue(uint256 newValue) public {
-        value = newValue;
-    }
-}
